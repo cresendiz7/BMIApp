06b922039ff6083e8b642f421c53886793db7aaa
cresendiz7 <cresendiz7@gmail.com>
2017-04-05 00:28:44 -0400

    fixes

--- app/src/main/java/com/christianresendiz/bmiapp/MainActivity.java
+++ app/src/main/java/com/christianresendiz/bmiapp/MainActivity.java
@@ -3,7 +3,6 @@ package com.christianresendiz.bmiapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
